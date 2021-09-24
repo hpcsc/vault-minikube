67a6eaa9263101a22587c09cd3f25f3a49f17170
David Nguyen <hpcsc@users.noreply.github.com>
2021-09-24 20:26:35 +1000

    Set vault k8s auth issuer

--- kubernetes/configmap-files/vault-init.sh
+++ kubernetes/configmap-files/vault-init.sh
@@ -30,14 +30,20 @@ function wait_for() {
 wait_for
 
 if [ -z "$(vault auth list | grep kubernetes)" ]; then
-  SA_JWT_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-  SA_CA_CRT=$(cat /var/run/secrets/kubernetes.io/serviceaccount/ca.crt)
+    SA_JWT_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+    SA_CA_CRT=$(cat /var/run/secrets/kubernetes.io/serviceaccount/ca.crt)
+    SA_ISSUER=$( wget --header "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+                    --no-check-certificate \
+                    -qO- https://kubernetes.default:443/.well-known/openid-configuration |
+                sed 's/.*"issuer":"\([^"]*\)".*/\1/g')
 
-  vault auth enable -path=kubernetes/minikube kubernetes
-  vault write auth/kubernetes/minikube/config \
-      token_reviewer_jwt="${SA_JWT_TOKEN}" \
-      kubernetes_host=https://kubernetes.default \
-      kubernetes_ca_cert="${SA_CA_CRT}"
+    vault auth enable -path=kubernetes/minikube kubernetes
+    vault write auth/kubernetes/minikube/config \
+        token_reviewer_jwt="${SA_JWT_TOKEN}" \
+        kubernetes_host=https://kubernetes.default \
+        kubernetes_ca_cert="${SA_CA_CRT}" \
+        disable_local_ca_jwt=true \
+        issuer="${SA_ISSUER}" # required for k8s 1.21
 fi
 vault policy write read-only /vault-init-config/policy.hcl
 vault write auth/kubernetes/minikube/role/read-only \
